f99f57cd4e69256cb8388a469aec160eb891ab14
Ahmad-Hamdy-Elhendawy <ahmad.hamdy.elhendawy@gmail.com>
2025-05-13 05:46:49 +0100

    Your commit message

--- ConsoleApplication2/Basic String Manipulation And Operation.cpp
+++ ConsoleApplication2/Basic String Manipulation And Operation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 string ReadString()
@@ -128,22 +129,191 @@ void CountThemAll(string str)
     cout << "All letters count: " << Ccounter + Lcounter << endl;
 }
 
+void CountCase(char c, string str, bool CaseSens = true)
+{
+    short counter = 0;
+    for (short i = 0; i < str.length(); i++)
+    {
+        if (CaseSens)
+        {
+            if (str[i] == c)
+                counter++;
+        }
+        else
+        {
+            if (tolower(c) == tolower(str[i]))
+                counter++;
+        }
+    }
+    cout << c << " exists " << counter << " times." << endl;
+   
+}
+
+bool IsVowel(char a)
+{
+    char c = tolower(a);
+    return  ((c == 'a') ||  (c == 'e') ||  (c == 'u') ||  (c == 'i') ||  (c == 'o'));
+}
+
+short CountVowel(char c, string str)
+{
+    short counter = 0;
+    for (short i = 0; i < str.length(); i++)
+    {
+        if (IsVowel(str[i]))
+            counter++;
+    }
+    return counter;
+}
+
+void PrintWords(string str)
+{
+    string space = " ";
+    short pos = 0;
+    short count = 0;
+    while ((pos = str.find(space)) != std::string::npos)
+    {
+        string sub = str.substr(0, pos);
+        if (sub != "")
+        {
+            cout << sub << endl;
+        }
+
+
+        str.erase(0, pos + space.length());
+    }
+    if (str != "")
+    {
+        cout << str << endl;
+
+    }
+    return;
+}
+
+short CountWords(string str)
+{
+    string space = " ";
+    short pos = 0;
+    short count = 0;
+    while ((pos = str.find(space)) != std::string::npos)
+    {
+        string sub = str.substr(0, pos);
+        if (sub != "")
+        {
+            count++;
+        }
+
+
+        str.erase(0, pos + space.length());
+    }
+    if (str != "")
+    {
+        count++;
+
+    }
+    return count;
+}
+
+void PrintVowels(char c, string str)
+{
+    for (short i = 0; i < str.length(); i++)
+    {
+        if (IsVowel(str[i]))
+            cout << str[i] << " ";
+    }
+    cout << endl;
+}
+
+vector<string> SplitString(string str, string n)
+{
+    vector<string> vec;
+    string dil = n;
+    short pos = 0;
+    short tokens = 0;
+    string sword = "";
+
+    while ((pos = str.find(dil)) != std::string::npos)
+    {
+        sword = str.substr(0, pos);
+        if (sword != "")
+        {
+            vec.push_back(sword);
+        tokens++;
+        str.erase(0, pos + dil.length());
+        }
+    }
+
+    if (str != "")
+    {
+        vec.push_back(str);
+        tokens++;
+    }
+    cout << "Tokens: " << tokens;
+    return vec;
+}
+
+string TrimLeft(string str)
+
+{
+    for (short i = 0; i < str.length() - 1; i++)
+    {
+        if (str[i] != ' ')
+            return str.substr(i, str.length() - i);
+    }
+}
+
+string TrimRight(string str)
+{
+    for (short i = str.length() - 1; i >= 0; i--)
+    {
+        if (str[i] != ' ')
+            return str.substr(0, i + 1);
+
+    }
+    return str;
+}
+
+string Trim(string str)
+{
+    return TrimLeft(TrimRight(str));
+}
+
+string Join(vector <string> names, string del = " ")
+{
+    string s = "";
+    for (string& holder : names)
+    {
+        s += holder + del;
+    }
+    return s.substr(0, s.length() - del.length());
+}
+
+
+
+
+
 int main()
 {
-    string str = ReadString();
-    char c = ReadChar();
+    // str = ReadString();
+    //char c = ReadChar();
 
-    cout << endl;
-    CountLetter(c, str);
-    PrintFirstLetter(str);
+    vector <string> names = { "ahmad", "mohamed", "Black person" };
+    cout << Join(names, ",");
 
-    cout << "\nCapitalized first letters:\n" << CapitalizeFirstLetter(str) << endl;
-    cout << "\nLowercased first letters:\n" << LowerFirstLetter(str) << endl;
+    //cout << endl;
+    //CountLetter(c, str);
+    //PrintFirstLetter(str);
 
-    UpperAll(str);
-    LowerAll(str);
-    Invert(str);
-    CountThemAll(str);
+    //cout << "\nCapitalized first letters:\n" << CapitalizeFirstLetter(str) << endl;
+    //cout << "\nLowercased first letters:\n" << LowerFirstLetter(str) << endl;
+
+    //UpperAll(str);
+    //LowerAll(str);
+    //Invert(str);
+    //CountThemAll(str);
+
+    //CountCase(c, str);
+    //PrintWords(str);
 
     return 0;
 }
